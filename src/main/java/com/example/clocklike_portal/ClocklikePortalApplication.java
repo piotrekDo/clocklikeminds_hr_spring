a59e4a869e6c0df7243eaa032cfbf77a40b124b1
Piotrek Domagalski <piotr.domagalski@yahoo.com>
2024-02-02 09:08:26 +0100

    initial

---
+++ src/main/java/com/example/clocklike_portal/ClocklikePortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.clocklike_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClocklikePortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClocklikePortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clocklike_portal/ClocklikePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clocklike_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClocklikePortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
